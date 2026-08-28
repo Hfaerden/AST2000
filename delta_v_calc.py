f049ef24fd5a4c21a0e8890e151fa04d57fbddc3
natipotati00 <nataniel.s.bar@gmail.com>
2026-08-28 10:26:58 +0200

    oppdaterte sim_gas og begynte på delat_calc

--- delta_v_calc.py
+++ delta_v_calc.py
@@ -13,10 +13,11 @@ print(mission.spacecraft_area)
 
 F = 1
 delta_v = 1
-v = mission.speed
+v = 1
 fuel_cons = 1
 speed_boost = v + delta_v
-fuel_kg = 1
+fuel_start = 1
+fuel_kg = fuel_start
 wet_mass = mission.spacecraft_mass + fuel_kg
 dt = 10**(-4)
 time = 0
@@ -31,5 +32,9 @@ while v < speed_boost :
     
     time += dt
 
-print(f"time needed to achive delta_v {time}")
-print()
+
+if fuel_kg > 0:
+    print(f"time needed to achive delta_v: {time} amout of fuel needed {fuel_start - fuel_kg} ")
+ 
+else:
+    print(f"ran out of fuel. it would take {-fuel_kg}kg of more fuel to do that manouver")
