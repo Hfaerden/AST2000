81b8ed2bb26817afe543e9e8d525ea408601a03f
natipotati00 <nataniel.s.bar@gmail.com>
2026-09-01 14:32:44 +0200

    started part E

---
+++ rocket_launch_check_if_correct.py
@@ -0,0 +1,14 @@
+
+
+from ast2000tools.SpaceMission import SpaceMission as SM
+
+
+thrust = 1
+fuel_cons = 1
+Init_fuel = 1
+estimated_timetospace = 1
+launch_position = [1, 1, 1]
+
+
+SM.set_launch_parameters()
+
