560c63e1fcf64588eeb4db9090d35ec51d2be8f1
hfaerden <havardfarden@gmail.com>
2026-09-21 15:13:33 +0200

    plotted all planets, and changed simulation time so all planets orbit at least once

--- del2/numericalsol.py
+++ del2/numericalsol.py
@@ -12,37 +12,44 @@ seed = utils.get_seed('natanies')
 mission = SM(seed)  #setter opp mission-raketten
 system = SS(seed)   #setter opp solsystemet vårt
 
-pos = system.initial_positions
-vel = system.initial_velocities 
-starmass = system.star_mass
+pos = system.initial_positions #Henter posisjonene ved t = 0
+vel = system.initial_velocities #Henter hastighetene ved t = 0
+starmass = system.star_mass #Henter solmassen vår 
 
 
-yearlen = system.semi_major_axes[0]**3/2
-timesteps_per_year = 10_000
-dt = yearlen/(timesteps_per_year)
-timesteps = round(20 * timesteps_per_year)
+yearlen = system.semi_major_axes[0]**(3/2) #Regner ut lengden på et år i jordår, ved Kepler's tredje
+print(yearlen)
+timesteps_per_year = 10_000 #Setter hvor mange tidssteg per år 
+dt = yearlen/(timesteps_per_year) #Regner ut tidsintervalet vi skal ved dt og årlengden
+timesteps = round(50 * timesteps_per_year) #Regner ut hvor mange tidssteg vi trenger totalt for 20 år 
 
-@jit
+@jit 
 def solve(index):
-    x1 = pos[0][index]
+    x1 = pos[0][index] #Henter posisjonene ved t = 0
     y1 = pos[1][index]
-    ax1 = -(4*np.pi**2 * starmass / (x1**2+y1**2))
+    ax1 = -(4*np.pi**2 * starmass / (x1**2+y1**2)) #Regner ut aksellerasjonen ved t = 0, merk at y = 0, og dermed ser uttrykket annerledes ut
     x = [x1]
-    y = [y1]
+    y = [y1] #Lager arrays for poisjonene våre 
     ax = [ax1]
     ay = [0]
-    vx = [vel[0][index]]
+    vx = [vel[0][index]] #lager arrays for hastighetene våre 
     vy = [vel[1][index]]
-    for i in range(timesteps - 1):
-        r = np.sqrt(x[i]**2 + y[i]**2)
-        ax.append(abs(x[i]/(r)) * 4*np.pi**2 * -np.sign(x[i]) * starmass / (r**2))
-        ay.append(abs(y[i]/(r)) * 4*np.pi**2 * -np.sign(y[i]) * starmass / (r**2))
-        vx.append(vx[i] + dt*ax[i+1])
-        vy.append(vy[i] + dt*ay[i+1])
-        x.append(x[i] + dt*vx[i+1])
-        y.append(y[i] + dt*vy[i+1])
-    return([x, y])
+    for i in range(timesteps - 1): #Euler-cromer loop for å regne ut aksellerasjon, posisjon, og hastighet
+        r = np.sqrt(x[i]**2 + y[i]**2) #Regner ut distansen fra sola 
+        ax.append(abs(x[i]/(r)) * 4*np.pi**2 * -np.sign(x[i]) * starmass / (r**2)) #Regner ut aksellerasjon i x retning og legger til i array
+        ay.append(abs(y[i]/(r)) * 4*np.pi**2 * -np.sign(y[i]) * starmass / (r**2)) #Regner ut aksellerasjon i y retning og legger til i array
+        vx.append(vx[i] + dt*ax[i+1]) #Regner ut hastighet i x retning og legger til i array
+        vy.append(vy[i] + dt*ay[i+1]) #Regner ut hastighet i y retning og legger til i array
+        x.append(x[i] + dt*vx[i+1]) #Regner ut posisjonen i x retning og legger til i array 
+        y.append(y[i] + dt*vy[i+1]) #Regner it posisjonen i y retning og legger til i array 
+    return([x, y]) #Returnerer x og y posisjonene 
 
-result = solve(0)
-plt.plot(result[0], result[1])
+results = []
+
+for i in range(len(system.radii)):
+    results.append(solve(i))
+for j in range(len(results)):
+    plt.plot(results[j][0], results[j][1])
+#plt.plot(result[0], result[1])
+print(len(system.radii))
 plt.show()
