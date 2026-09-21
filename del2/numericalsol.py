11419be3ccc6e9307254e40cf8d4040615cbc112
hfaerden <havardfarden@gmail.com>
2026-09-21 13:57:23 +0200

    fixed code :)

--- del2/numericalsol.py
+++ del2/numericalsol.py
@@ -23,22 +23,22 @@ def solve(index):
     x1 = pos[0][index]
     y1 = pos[1][index]
     ax1 = -(4*np.pi**2 * starmass / (x1**2+y1**2))
-    #ay1 = -np.sin(np.arctan(y1/x1))* (4*np.pi**2 * starmass / (x1**2+y1**2))
     x = [x1]
     y = [y1]
     ax = [ax1]
     ay = [0]
-    vx = [vel[index][0]]
-    vy = [vel[index][1]]
+    vx = [vel[0][index]]
+    vy = [vel[1][index]]
     for i in range(timesteps - 1):
         r = np.sqrt(x[i]**2 + y[i]**2)
-        ax.append((x[i]/(r)) * 4*np.pi**2 * ((x[i])+10**(-11))/abs(x[i]+10**(-11)) * starmass / (r**2))
-        ay.append((y[i]/(r)) * 4*np.pi**2 * ((y[i])+10**(-11))/abs(y[i]+10**(-11)) * starmass / (r**2))
+        ax.append(abs(x[i]/(r)) * 4*np.pi**2 * -np.sign(x[i]) * starmass / (r**2))
+        ay.append(abs(y[i]/(r)) * 4*np.pi**2 * -np.sign(y[i]) * starmass / (r**2))
         vx.append(vx[i] + dt*ax[i+1])
         vy.append(vy[i] + dt*ay[i+1])
         x.append(x[i] + dt*vx[i+1])
         y.append(y[i] + dt*vy[i+1])
-    return(x)
+    return([x, y])
 
-plt.plot(range(timesteps), solve(0))
+result = solve(0)
+plt.plot(result[0], result[1])
 plt.show()
